68340db14c62aa65978d8deb408f2a6c3fec705c
matheus-arj <matheus.arj@hotmail.com>
2024-05-20 23:31:18 -0300

    created db config file

---
+++ dbconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'platform',
+  password: process.env.DB_PASSWORD || 'postgres',
+  port: process.env.DB_PORT || 5432,
+};
