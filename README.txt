519c73dc0a2aedd16bb4e134eeb2902d1100a306
matheus-arj <matheus.arj@hotmail.com>
2025-02-16 23:52:56 -0300

    feat: added all the methods of customer storage

---
+++ README.txt
@@ -0,0 +1,5 @@
+need to install postgresql
+MAC OS: 
+brew install postgresql
+Linux: 
+apt install postgresql
--- src/customers/storage/customer.py
+++ src/customers/storage/customer.py
@@ -1,5 +1,6 @@
 import psycopg2
 import logging
+from domain.customer import CustomerDomain
 
 logger = logging.getLogger(__name__)
 
@@ -34,3 +35,77 @@ class CustomerStorage:
         ''')
 
         self.connection.commit()
+
+    def get(self, id: int) -> CustomerDomain:
+        logging.debug(f"[CUSTOMER-STORAGE] Getting customer with ID: {id}")
+        db = self.connection.cursor()
+        db.execute('''
+            SELECT
+                id, name, email
+            FROM
+                  customers
+            WHERE id = ?
+        ''', (id,))
+        customer = db.fetchone()
+        logging.info(f"[CUSTOMER-STORAGE] Customer retrieved: {customer}")
+        ret = CustomerDomain(customer[1], customer[2])
+        ret.set_id(customer[0])
+        return ret
+    
+    def get_all(self) -> list:
+        logging.debug("[CUSTOMER-STORAGE] Getting all customers")
+        db = self.connection.cursor()
+        db.execute('''
+            SELECT
+                id, name, email
+            FROM
+                customers
+        ''')
+        customers = db.fetchall()
+        logging.debug(f"[CUSTOMER-STORAGE] All customers retrieved")
+        ret = []
+        
+        for customer in customers:
+            logger.debug(f"[CUSTOMER-STORAGE] data: {customer}")
+            item = CustomerDomain(customer[1], customer[2])
+            item.set_id(customer[0])
+            ret.append(item)
+        
+        return ret
+    
+    def add(self, customer: CustomerDomain) -> int:
+        logging.debug(f"[CUSTOMER-STORAGE] Adding customer")
+        db = self.connection.cursor()
+        db.execute('''
+            INSERT INTO customers (name, email)
+            VALUES (?, ?)
+        ''', (customer.name, customer.email))
+
+        self.connection.commit()
+        
+        return db.lastrowid
+
+    def update(self, customer: CustomerDomain) -> bool:
+        logging.debug(f"[CUSTOMER-STORAGE] Updating customer")
+        db = self.connection.cursor()
+        db.execute('''
+            UPDATE customers
+            SET name = ?, email = ?
+            WHERE id = ?
+        ''', (customer.name, customer.email, customer.customer_id))
+        
+        self.connection.commit()
+
+        return db.rowcount > 0
+    
+    def delete(self, id: int) -> bool:
+        logging.debug(f"[CUSTOMER-STORAGE] Deleting customer with Id: {id}")
+        db = self.connection.cursor()
+        db.execute('''
+            DELETE FROM customers
+            WHERE id = ?
+        ''', (id))
+
+        self.connection.commit()
+        
+        return db.rowcount > 0
