b2bfcafa277431951565a569d9688e4a27b51762
matheus-arj <matheus.arj@hotmail.com>
2025-03-12 23:18:21 -0300

    feat: changed customer id to id

--- src/customers/domain/customer_domain.py
+++ src/customers/domain/customer_domain.py
@@ -1,18 +1,18 @@
 class CustomerDomain:
     def __init__(self, name: str, email: str):
-        self.customer_id = None
+        self.id = None
         self.name = name
         self.email = email
 
     def set_id(self, customer_id: int):
-        self.customer_id = customer_id
+        self.id = customer_id
 
     def __str__(self):
-        return f'Customer ID: {self.customer_id}, Name: {self.name}, Email: {self.email}'
+        return f'Customer ID: {self.id}, Name: {self.name}, Email: {self.email}'
     
     def to_json(self):
         return {
-            'customer_id': self.customer_id,
+            'id': self.id,
             'name': self.name,
             'email': self.email
         }
